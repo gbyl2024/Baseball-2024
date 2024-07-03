df1451334b17a24ec10edf4ae8f87af1757b61fc
doyun.kim <gbyl2024@gmail.com>
2024-07-03 14:55:23 +0900

    [feature] 25p

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
+#include "../Project20/baseball.cpp"
+#include<stdexcept>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
