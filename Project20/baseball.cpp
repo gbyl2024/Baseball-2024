aa53550503613be3cb2560d975fd321915ffb5ba
doyun.kim <gbyl2024@gmail.com>
2024-07-03 15:01:27 +0900

    [feature] 29p

--- Project20/baseball.cpp
+++ Project20/baseball.cpp
@@ -7,5 +7,11 @@ public:
 		if (string.length() != 3) {
 			throw length_error("Must be three letter");
 		}
+
+		for (char ch : string) {
+			if (ch < '0' || ch > '9') {
+				throw invalid_argument("Mus be number");
+			}
+		}
 	}
 };
